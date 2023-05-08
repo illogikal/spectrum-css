946d115e83db0f516efc2be7590feebf4d792246
Megan Logsdon <megan@heysparkbox.com>
2023-05-08 14:36:43 -0400

    feat(tabs): updating styling to use tokens

--- components/tabs/stories/template.js
+++ components/tabs/stories/template.js
@@ -7,7 +7,6 @@ import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { Template as Icon } from "@spectrum-css/icon/stories/template.js";
 
 import "../index.css";
-import "../skin.css";
 
 export const Template = ({
   rootClass = "spectrum-Tabs",
