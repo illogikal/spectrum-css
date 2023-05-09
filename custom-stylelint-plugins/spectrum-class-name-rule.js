cf033dd462843f1b811b66f3025d449da6d38390
Rajdeep Chandra <rajdeepc@adobe.com>
2023-05-09 18:03:16 +0530

    chore(stylelint): classname SUIT naming style stylelint

---
+++ custom-stylelint-plugins/spectrum-class-name-rule.js
@@ -0,0 +1,33 @@
+const stylelint = require('stylelint');
+
+const ruleName = 'custom-rule/spectrum-class-name';
+const messages = stylelint.utils.ruleMessages(ruleName, {
+  wrongCapitalization: (className) =>
+    `Class name "${className}" should follow the SUIT-style naming conventions with the ".spectrum" prefix.`,
+});
+
+const suitPattern = /^\.spectrum-[A-Z][a-zA-Z0-9]*(-[a-zA-Z0-9]+)*$/;
+
+module.exports = stylelint.createPlugin(ruleName, (enabled) => {
+  return (root, result) => {
+    if (!enabled) {
+      return;
+    }
+
+    root.walkRules((rule) => {
+      rule.selectors.forEach((selector) => {
+        if (suitPattern.test(selector) === false) {
+          stylelint.utils.report({
+            ruleName,
+            result,
+            node: rule,
+            message: messages.wrongCapitalization(selector),
+          });
+        }
+      });
+    });
+  };
+});
+
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
