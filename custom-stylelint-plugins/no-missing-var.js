f24c2e8ed6287b94e1d1564358b790f6f2a4a2f7
Rajdeep Chandra <rajdeepc@adobe.com>
2023-05-10 22:54:13 +0530

    fix: no var lint rule

--- custom-stylelint-plugins/no-missing-var.js
+++ custom-stylelint-plugins/no-missing-var.js
@@ -5,13 +5,16 @@ const ruleName = 'custom-rule/no-missing-var';
 const plugin = stylelint.createPlugin(ruleName, (enabled, options) => {
   return (root, result) => {
     root.walkDecls((decl) => {
-      const value = decl.value.replace(/\s/g, '');
-      const regex = /(?<!var\()(--\S+)\)/g;
+      const value = decl.value.replace(/\s/g, ''); // Remove whitespace
+      const regex = /(?<!var\()--\S+\b/g;
       let match;
       while ((match = regex.exec(value)) !== null) {
-        const varName = match[1];
-        const message = `Missing 'var' before custom property "${varName}"`;
-        stylelint.utils.report({ message, node: decl, result, ruleName });
+        const varName = match[0];
+        const isIgnored = decl.parent.type === 'atrule' && decl.parent.name === 'remapvars' && decl.parent.toString().includes(varName);
+        if (!isIgnored) {
+          const message = `Missing 'var' before custom property "${varName}"`;
+          stylelint.utils.report({ message, node: decl, result, ruleName });
+        }
       }
     });
   };
